9c31105e11721f1bc8341c24a3dbfda6fa1d0a04
nice <nice@dcpt.online>
2019-10-31 16:56:20 +0800

    123

---
+++ docs/precache-manifest.e7d61744201f27ee990edff2f16428e2.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65be4fd48f64463a901af88bb9791a16",
+    "url": "index.html"
+  },
+  {
+    "revision": "a17fd50df63cfe6739f80f81e4dba29c",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "29c3810fa79a1139c66f",
+    "url": "static/css/app.cce8fd10.css"
+  },
+  {
+    "revision": "613e8305a5da5f2bdfe1",
+    "url": "static/css/chunk-vendors.14aaf9bb.css"
+  },
+  {
+    "revision": "868b60cbd6a836af0f1ddce8cbd139bc",
+    "url": "static/img/background.868b60cb.png"
+  },
+  {
+    "revision": "a1a2968db96ac582abf7513f6c36389f",
+    "url": "static/img/banner1.a1a2968d.png"
+  },
+  {
+    "revision": "cf600040e243b1e4a2f0c7f1b48b9561",
+    "url": "static/img/banner2.cf600040.png"
+  },
+  {
+    "revision": "ff5ae8cdaf3710e3a0bc30494b5a5987",
+    "url": "static/img/info.ff5ae8cd.png"
+  },
+  {
+    "revision": "6d9b59fbd210f28085768dfa196f61c7",
+    "url": "static/img/logo.6d9b59fb.png"
+  },
+  {
+    "revision": "2a3d4ae45ad4d9791397843c54f67dec",
+    "url": "static/img/play1s.2a3d4ae4.png"
+  },
+  {
+    "revision": "af54564c93c9885447100b3eec962c71",
+    "url": "static/img/play2s.af54564c.png"
+  },
+  {
+    "revision": "9745ea08b928e5278797c6bdf5fdff9f",
+    "url": "static/img/play3s.9745ea08.png"
+  },
+  {
+    "revision": "f53ae65035fef4fb4bb6abbc6e977b65",
+    "url": "static/img/play4s.f53ae650.png"
+  },
+  {
+    "revision": "de1981e53d3e191aa30955957ad32859",
+    "url": "static/img/video1.de1981e5.png"
+  },
+  {
+    "revision": "f557361d9c0f0248ccdd94c10ba7115a",
+    "url": "static/img/video2.f557361d.png"
+  },
+  {
+    "revision": "8e220c3bfef72d0c23a879ccaff6306c",
+    "url": "static/img/video3.8e220c3b.png"
+  },
+  {
+    "revision": "7173c1fad727069ceb869c5ba2d59219",
+    "url": "static/img/video4.7173c1fa.png"
+  },
+  {
+    "revision": "4a9578ddfcca6239b5197df2a7085166",
+    "url": "static/img/win1.4a9578dd.png"
+  },
+  {
+    "revision": "627ecaf50efa7684b6c49c2a6db58087",
+    "url": "static/img/win2.627ecaf5.png"
+  },
+  {
+    "revision": "29c3810fa79a1139c66f",
+    "url": "static/js/app.213664af.js"
+  },
+  {
+    "revision": "613e8305a5da5f2bdfe1",
+    "url": "static/js/chunk-vendors.27807c12.js"
+  }
+]);
