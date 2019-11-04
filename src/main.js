8d02b8c8b8288176a55b5b3aecb3848595c6901c
nice <nice@dcpt.online>
2019-11-04 20:33:40 +0800

    123

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// import './registerServiceWorker'
+import './registerServiceWorker'
 import Vant from 'vant'
 import 'vant/lib/index.css';
 import './assets/style/reset.less'
